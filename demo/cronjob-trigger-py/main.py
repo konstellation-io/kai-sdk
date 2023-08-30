4981adfbd529105f9d8809cddf2f255a8782215d
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-08-30 16:09:34 +0200

    fix: pylint

--- demo/cronjob-trigger-py/main.py
+++ demo/cronjob-trigger-py/main.py
@@ -1,9 +1,11 @@
-from sdk import kai_sdk as sdk
-from runner.trigger import trigger_runner
-from runner.runner import Runner
 import asyncio
 import time
+
 from google.protobuf.wrappers_pb2 import StringValue
+
+from runner.runner import Runner
+from runner.trigger import trigger_runner
+from sdk import kai_sdk as sdk
 
 
 async def initializer(sdk: sdk.KaiSDK):
@@ -13,18 +15,28 @@ async def initializer(sdk: sdk.KaiSDK):
     sdk.metadata.logger.info(f"product {sdk.metadata.get_product()}")
     sdk.metadata.logger.info(f"workflow {sdk.metadata.get_workflow()}")
     sdk.metadata.logger.info(f"version {sdk.metadata.get_version()}")
-    sdk.metadata.logger.info(f"kv_product {sdk.metadata.get_key_value_store_product_name()}")
-    sdk.metadata.logger.info(f"kv_workflow {sdk.metadata.get_key_value_store_workflow_name()}")
-    sdk.metadata.logger.info(f"kv_process {sdk.metadata.get_key_value_store_process_name()}")
+    sdk.metadata.logger.info(
+        f"kv_product {sdk.metadata.get_key_value_store_product_name()}"
+    )
+    sdk.metadata.logger.info(
+        f"kv_workflow {sdk.metadata.get_key_value_store_workflow_name()}"
+    )
+    sdk.metadata.logger.info(
+        f"kv_process {sdk.metadata.get_key_value_store_process_name()}"
+    )
     sdk.metadata.logger.info(f"object-store {sdk.metadata.get_object_store_name()}")
 
     sdk.path_utils.logger.info(f"base path {sdk.path_utils.get_base_path()}")
-    sdk.path_utils.logger.info(f"compose base path {sdk.path_utils.compose_path('test')}")
+    sdk.path_utils.logger.info(
+        f"compose base path {sdk.path_utils.compose_path('test')}"
+    )
 
     value1 = await sdk.centralized_config.get_config("test1")
     value2 = await sdk.centralized_config.get_config("test2")
 
-    sdk.centralized_config.logger.info(f"config values from comfig.yaml test1: {value1} test2: {value2}")
+    sdk.centralized_config.logger.info(
+        f"config values from comfig.yaml test1: {value1} test2: {value2}"
+    )
 
 
 async def cronjob_runner(trigger_runner: trigger_runner.TriggerRunner, sdk: sdk.KaiSDK):
@@ -34,9 +46,11 @@ async def cronjob_runner(trigger_runner: trigger_runner.TriggerRunner, sdk: sdk.
         await sdk.messaging.send_output(StringValue(value="hello world"))
         time.sleep(2)
 
+
 def finalizer(sdk: sdk.KaiSDK):
     logger = sdk.logger.bind(context="[CRONJOB FINALIZER]")
     logger.info("finalizing example...")
+
 
 def handler(sdk: sdk.KaiSDK, response: StringValue):
     logger = sdk.logger.bind(context="[CRONJOB HANDLER]")
@@ -45,11 +59,14 @@ def handler(sdk: sdk.KaiSDK, response: StringValue):
 
 async def init():
     runner = await Runner().initialize()
-    await runner.trigger_runner().with_initializer(initializer).with_runner(cronjob_runner).with_finalizer(finalizer).run()
-    #await runner.task_runner().with_initializer(initializer).with_handler(handler).with_finalizer(finalizer).run()
-    #await runner.exit_runner().with_initializer(initializer).with_handler(handler).with_finalizer(finalizer).run()
+    await runner.trigger_runner().with_initializer(initializer).with_runner(
+        cronjob_runner
+    ).with_finalizer(finalizer).run()
+    # await runner.task_runner().with_initializer(initializer).with_handler(handler).with_finalizer(finalizer).run()
+    # await runner.exit_runner().with_initializer(initializer).with_handler(handler).with_finalizer(finalizer).run()
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(init())
     loop.run_forever()
