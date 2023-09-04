d955c6be9da014699b8b6422d908c9e4d3a49d83
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-09-04 12:58:09 +0200

    fix: create a new event loop for the demo, set localhost as nats default url

--- demo/cronjob-trigger-py/main.py
+++ demo/cronjob-trigger-py/main.py
@@ -76,7 +76,7 @@ async def init():
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     loop.run_until_complete(init())
     loop.run_forever()
     loop.close()
