8c580e523f477ff90b784dfe6718f5c2b99c6f74
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-09-25 14:47:18 +0200

    feat: splitter node

--- demo/training-workflow/src/splitter/main.py
+++ demo/training-workflow/src/splitter/main.py
@@ -4,62 +4,47 @@ from google.protobuf.any_pb2 import Any
 from google.protobuf.wrappers_pb2 import StringValue
 from runner.runner import Runner
 from sdk import kai_sdk as sdk
-from sdk.centralized_config.centralized_config import Scope
+from proto.training_pb2 import Training, Splitter
+
+
+import random
+import string
+
+def get_random_string(length: int) -> str:
+    letters = string.ascii_lowercase
+    return (''.join(random.choice(letters) for _ in range(length)))
 
 
 async def initializer(sdk: sdk.KaiSDK):
-    logger = sdk.logger.bind(context="[TASK INITIALIZER]")
-    logger.info("starting example...")
-    value, _ = await sdk.centralized_config.get_config("test")
-    if value is None:
-        logger.info("config value not found")
-    else:
-        logger.info(f"config value retrieved! {value}")
-
-    value, _ = await sdk.object_store.get("test")
-    if value is None:
-        logger.info("object store value not found")
-    else:
-        logger.info(f"object store value retrieved! {value.decode('utf-8')}")
+    logger = sdk.logger.bind(context="[SPLITTER INITIALIZER]")
+    logger.info("starting process...")
 
 
 async def handler(sdk: sdk.KaiSDK, response: Any):
-    logger = sdk.logger.bind(context="[TASK HANDLER]")
-    string_value = StringValue()
+    logger = sdk.logger.bind(context="[SPLITTER HANDLER]")
+    input_proto = Splitter()
 
-    response.Unpack(string_value)
-    message = string_value.value
-    logger.info(f"received message {message}")
+    response.Unpack(input_proto)
+    logger.info(f"received message {input_proto.repo_url}")
 
-    composed_string = f"{message}, processed by the task process!"
-    logger.info(f"sending message {composed_string}")
-    await sdk.messaging.send_output(StringValue(value=composed_string))
+    output = Training(
+        training_id=input_proto.training_id,
+        model_id=get_random_string(random.randint(10, 20))
+    )
+    logger.info(f"sending message {output}")
 
-
-def preprocessor(sdk: sdk.KaiSDK, response: Any):
-    logger = sdk.logger.bind(context="[TASK PREPROCESSOR]")
-    logger.info("I am a sync preprocessor")
-
-
-async def postprocessor(sdk: sdk.KaiSDK, response: Any):
-    logger = sdk.logger.bind(context="[TASK POSTPROCESSOR]")
-    logger.info("I am an async postprocessor")
-    await asyncio.sleep(0.0000001)
-    logger.info("I am an async postprocessor, after sleep")
+    await sdk.messaging.send_output(StringValue(value=output))
+    logger.info("splitted task received")
 
 
 def finalizer(sdk: sdk.KaiSDK):
-    logger = sdk.logger.bind(context="[TASK FINALIZER]")
-    logger.info("finalizing example synchronously...")
+    logger = sdk.logger.bind(context="[SPLITTER FINALIZER]")
+    logger.info("finalizing process...")
 
 
 async def init():
     runner = await Runner().initialize()
-    await runner.task_runner().with_initializer(initializer).with_handler(
-        handler
-    ).with_finalizer(finalizer).with_preprocessor(preprocessor).with_postprocessor(
-        postprocessor
-    ).run()
+    await runner.task_runner().with_initializer(initializer).with_handler(handler).with_finalizer(finalizer).run()
 
 
 if __name__ == "__main__":
