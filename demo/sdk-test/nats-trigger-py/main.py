ccb50e702801e217aa25a437a695d47ba4ca0b81
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-10-02 15:17:14 +0200

    feat: outdated method

--- demo/sdk-test/nats-trigger-py/main.py
+++ demo/sdk-test/nats-trigger-py/main.py
@@ -49,25 +49,6 @@ async def initializer(sdk: sdk.KaiSDK):
         f"config values from comfig.yaml test1: {value1} test2: {value2}"
     )
 
-async def process_message(trigger_runner: trigger_runner.TriggerRunner, sdk: sdk.KaiSDK, msg: Msg):
-    logger = sdk.logger.bind(context="[NATS SUBSCRIBER CALLBACK]")
-    logger.info("processing message...")
-
-    request_id = str(uuid.uuid4())
-    await sdk.messaging.send_output_with_request_id(
-        StringValue(value="Hi there, I'm a NATS subscriber!"),
-        request_id
-    )
-
-    response_channel = await trigger_runner.get_response_channel(request_id)
-
-    logger.info(f"message received: {response_channel}")
-
-    try:
-        await msg.ack()
-    except Exception as e:
-        logger.error(f"error acknowledging message: {e}")
-
 async def nats_subscriber_runner(trigger_runner: trigger_runner.TriggerRunner, sdk: sdk.KaiSDK):
     logger = sdk.logger.bind(context="[NATS SUBSCRIBER RUNNER]")
     logger.info("executing example...")
