d13671b938c1ffde246c6e66af210498c3035405
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-10-05 10:59:44 +0200

    feat: work in progress nats trigger

---
+++ demo/nats-trigger-py/main.py
@@ -0,0 +1,116 @@
+import asyncio
+import uuid
+from datetime import timedelta
+
+from google.protobuf.wrappers_pb2 import StringValue
+from nats.aio.client import Client as NatsClient
+from nats.aio.msg import Msg
+from nats.js.api import ConsumerConfig, DeliverPolicy
+from nats.js.client import JetStreamContext
+from runner.runner import Runner
+from runner.trigger import trigger_runner
+from sdk import kai_sdk as sdk
+from sdk.centralized_config.centralized_config import Scope
+
+
+async def initializer(sdk: sdk.KaiSDK):
+    logger = sdk.logger.bind(context="[NATS SUBSCRIBER INITIALIZER]")
+    logger.info("starting example...")
+    sdk.metadata.logger.info(f"process {sdk.metadata.get_process()}")
+    sdk.metadata.logger.info(f"product {sdk.metadata.get_product()}")
+    sdk.metadata.logger.info(f"workflow {sdk.metadata.get_workflow()}")
+    sdk.metadata.logger.info(f"version {sdk.metadata.get_version()}")
+    sdk.metadata.logger.info(
+        f"kv_product {sdk.metadata.get_key_value_store_product_name()}"
+    )
+    sdk.metadata.logger.info(
+        f"kv_workflow {sdk.metadata.get_key_value_store_workflow_name()}"
+    )
+    sdk.metadata.logger.info(
+        f"kv_process {sdk.metadata.get_key_value_store_process_name()}"
+    )
+    sdk.metadata.logger.info(f"object-store {sdk.metadata.get_object_store_name()}")
+
+    sdk.path_utils.logger.info(f"base path {sdk.path_utils.get_base_path()}")
+    sdk.path_utils.logger.info(
+        f"compose base path {sdk.path_utils.compose_path('test')}"
+    )
+
+    value1 = await sdk.centralized_config.get_config("test1")
+    value2 = await sdk.centralized_config.get_config("test2")
+
+    await sdk.centralized_config.set_config("test", "value", Scope.WorkflowScope)
+    await sdk.centralized_config.set_config(
+        "nats.url", "nats://localhost:4222", Scope.ProcessScope
+    )
+
+    await sdk.object_store.save("test", bytes("value-obj", "utf-8"))
+
+    sdk.centralized_config.logger.info(
+        f"config values from comfig.yaml test1: {value1} test2: {value2}"
+    )
+
+
+async def nats_subscriber_runner(
+    trigger_runner: trigger_runner.TriggerRunner, sdk: sdk.KaiSDK
+):
+    logger = sdk.logger.bind(context="[NATS SUBSCRIBER RUNNER]")
+    logger.info("executing example...")
+
+    nats_url = await sdk.centralized_config.get_config("nats.url")
+    logger.info(f"nats url: {nats_url[0]}")
+    nc: NatsClient = NatsClient()
+    js: JetStreamContext = nc.jetstream()
+    await nc.connect(nats_url[0])
+    ack_time = timedelta(hours=22).total_seconds()
+
+    async def process_message(msg: Msg):
+        logger = sdk.logger.bind(context="[NATS SUBSCRIBER CALLBACK]")
+        logger.info("processing message...")
+
+        request_id = str(uuid.uuid4())
+        await sdk.messaging.send_output_with_request_id(
+            StringValue(value="Hi there, I'm a NATS subscriber!"), request_id
+        )
+
+        response_channel = await trigger_runner.get_response_channel(request_id)
+
+        logger.info(f"message received: {response_channel}")
+
+        try:
+            await msg.ack()
+        except Exception as e:
+            logger.error(f"error acknowledging message: {e}")
+
+    logger.info("subscribing to nats-trigger...")
+    try:
+        await js.subscribe(
+            subject="nats-trigger",
+            queue="nats-trigger-queue",
+            durable="nats-trigger",
+            cb=process_message,
+            config=ConsumerConfig(deliver_policy=DeliverPolicy.NEW, ack_wait=ack_time),
+            manual_ack=True,
+        )
+    except Exception as e:
+        logger.error(f"error subscribing to the NATS subject nats-trigger: {e}")
+        sys.exit(1)
+
+
+def finalizer(sdk: sdk.KaiSDK):
+    logger = sdk.logger.bind(context="[NATS SUBSCRIBER FINALIZER]")
+    logger.info("finalizing example...")
+
+
+async def init():
+    runner = await Runner().initialize()
+    await runner.trigger_runner().with_initializer(initializer).with_runner(
+        nats_subscriber_runner
+    ).with_finalizer(finalizer).run()
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(init())
+    loop.run_forever()
+    loop.close()
