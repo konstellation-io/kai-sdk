4285aa433b699c3a0a473029370f69bfe9e202ba
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-10-05 11:29:03 +0200

    feat: update poetry and examples

--- demo/nats-trigger-py/main.py
+++ demo/nats-trigger-py/main.py
@@ -10,45 +10,11 @@ from nats.js.client import JetStreamContext
 from runner.runner import Runner
 from runner.trigger import trigger_runner
 from sdk import kai_sdk as sdk
-from sdk.centralized_config.centralized_config import Scope
 
 
 async def initializer(sdk: sdk.KaiSDK):
     logger = sdk.logger.bind(context="[NATS SUBSCRIBER INITIALIZER]")
     logger.info("starting example...")
-    sdk.metadata.logger.info(f"process {sdk.metadata.get_process()}")
-    sdk.metadata.logger.info(f"product {sdk.metadata.get_product()}")
-    sdk.metadata.logger.info(f"workflow {sdk.metadata.get_workflow()}")
-    sdk.metadata.logger.info(f"version {sdk.metadata.get_version()}")
-    sdk.metadata.logger.info(
-        f"kv_product {sdk.metadata.get_key_value_store_product_name()}"
-    )
-    sdk.metadata.logger.info(
-        f"kv_workflow {sdk.metadata.get_key_value_store_workflow_name()}"
-    )
-    sdk.metadata.logger.info(
-        f"kv_process {sdk.metadata.get_key_value_store_process_name()}"
-    )
-    sdk.metadata.logger.info(f"object-store {sdk.metadata.get_object_store_name()}")
-
-    sdk.path_utils.logger.info(f"base path {sdk.path_utils.get_base_path()}")
-    sdk.path_utils.logger.info(
-        f"compose base path {sdk.path_utils.compose_path('test')}"
-    )
-
-    value1 = await sdk.centralized_config.get_config("test1")
-    value2 = await sdk.centralized_config.get_config("test2")
-
-    await sdk.centralized_config.set_config("test", "value", Scope.WorkflowScope)
-    await sdk.centralized_config.set_config(
-        "nats.url", "nats://localhost:4222", Scope.ProcessScope
-    )
-
-    await sdk.object_store.save("test", bytes("value-obj", "utf-8"))
-
-    sdk.centralized_config.logger.info(
-        f"config values from comfig.yaml test1: {value1} test2: {value2}"
-    )
 
 
 async def nats_subscriber_runner(
@@ -57,11 +23,9 @@ async def nats_subscriber_runner(
     logger = sdk.logger.bind(context="[NATS SUBSCRIBER RUNNER]")
     logger.info("executing example...")
 
-    nats_url = await sdk.centralized_config.get_config("nats.url")
-    logger.info(f"nats url: {nats_url[0]}")
     nc: NatsClient = NatsClient()
     js: JetStreamContext = nc.jetstream()
-    await nc.connect(nats_url[0])
+    await nc.connect("nats://localhost:4222")
     ack_time = timedelta(hours=22).total_seconds()
 
     async def process_message(msg: Msg):
@@ -85,16 +49,16 @@ async def nats_subscriber_runner(
     logger.info("subscribing to nats-trigger...")
     try:
         await js.subscribe(
-            subject="nats-trigger",
-            queue="nats-trigger-queue",
-            durable="nats-trigger",
+            subject="demo-trigger",
+            queue="demo-trigger-queue",
+            durable="demo-trigger",
             cb=process_message,
             config=ConsumerConfig(deliver_policy=DeliverPolicy.NEW, ack_wait=ack_time),
             manual_ack=True,
         )
     except Exception as e:
-        logger.error(f"error subscribing to the NATS subject nats-trigger: {e}")
-        sys.exit(1)
+        logger.error(f"error subscribing to the NATS subject demo-trigger: {e}")
+        exit(1)
 
 
 def finalizer(sdk: sdk.KaiSDK):
--- demo/rest-trigger-py/main.py
+++ demo/rest-trigger-py/main.py
@@ -9,7 +9,6 @@ from google.protobuf.wrappers_pb2 import StringValue
 from runner.runner import Runner
 from runner.trigger import trigger_runner
 from sdk import kai_sdk as sdk
-from sdk.centralized_config.centralized_config import Scope
 
 app = FastAPI()
 import contextvars
@@ -21,37 +20,6 @@ async def initializer(sdk: sdk.KaiSDK):
     logger = sdk.logger.bind(context="[REST SERVER INITIALIZER]")
     logger.info("starting example...")
     sdk_instance.set(sdk)
-
-    sdk.metadata.logger.info(f"process {sdk.metadata.get_process()}")
-    sdk.metadata.logger.info(f"product {sdk.metadata.get_product()}")
-    sdk.metadata.logger.info(f"workflow {sdk.metadata.get_workflow()}")
-    sdk.metadata.logger.info(f"version {sdk.metadata.get_version()}")
-    sdk.metadata.logger.info(
-        f"kv_product {sdk.metadata.get_key_value_store_product_name()}"
-    )
-    sdk.metadata.logger.info(
-        f"kv_workflow {sdk.metadata.get_key_value_store_workflow_name()}"
-    )
-    sdk.metadata.logger.info(
-        f"kv_process {sdk.metadata.get_key_value_store_process_name()}"
-    )
-    sdk.metadata.logger.info(f"object-store {sdk.metadata.get_object_store_name()}")
-
-    sdk.path_utils.logger.info(f"base path {sdk.path_utils.get_base_path()}")
-    sdk.path_utils.logger.info(
-        f"compose base path {sdk.path_utils.compose_path('test')}"
-    )
-
-    value1 = await sdk.centralized_config.get_config("test1")
-    value2 = await sdk.centralized_config.get_config("test2")
-
-    await sdk.centralized_config.set_config("test", "value", Scope.WorkflowScope)
-
-    await sdk.object_store.save("test", bytes("value-obj", "utf-8"))
-
-    sdk.centralized_config.logger.info(
-        f"config values from comfig.yaml test1: {value1} test2: {value2}"
-    )
 
 
 async def rest_server_runner(
