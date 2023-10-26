4f383b93db44ee63e720a3fd10f0d2331a0cfc9f
kafkaphoenix <javier.aguilera@intelygenz.com>
2023-10-26 14:05:55 +0200

    feat(go-sdk): new method get request id given nats message (#70)

--- py-sdk/sdk/sdk/messaging/exceptions.py
+++ py-sdk/sdk/sdk/messaging/exceptions.py
@@ -10,3 +10,9 @@ class FailedToGetMaxMessageSizeError(Exception):
 class MessageTooLargeError(Exception):
     def __init__(self, message_size: str, max_message_size: str):
         super().__init__(f"message size {message_size} is larger than max message size {max_message_size}")
+
+
+class NewRequestMsgError(Exception):
+    def __init__(self, error: Optional[Exception] = None):
+        message = "error creating new request message"
+        super().__init__(f"{message}: {error}" if error else message)
