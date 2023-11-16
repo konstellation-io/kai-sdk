23801adce80cd04c44d21ea25309fa24d1e3a797
David Fuentes <51330112+dfinteligenz@users.noreply.github.com>
2023-11-16 10:08:48 +0100

    feat: authentication persistent storage PY (#92)

--- py-sdk/sdk/sdk/persistent_storage/persistent_storage_test.py
+++ py-sdk/sdk/sdk/persistent_storage/persistent_storage_test.py
@@ -1,4 +1,5 @@
 import io
+from datetime import datetime
 from unittest.mock import Mock, patch
 
 import pytest
@@ -6,15 +7,37 @@ import urllib3
 from minio import Minio
 from vyper import v
 
+from sdk.auth.authentication import Authentication
 from sdk.persistent_storage.exceptions import (
     FailedToDeleteFileError,
     FailedToGetFileError,
     FailedToInitializePersistentStorageError,
     FailedToSaveFileError,
 )
-from sdk.persistent_storage.persistent_storage import PersistentStorage, PersistentStorageABC
+from sdk.persistent_storage.persistent_storage import Object, ObjectInfo, PersistentStorage, PersistentStorageABC
 
 TTL_DAYS = 30
+EXPIRE_DATETIME = datetime.strptime("Sat, 18 Nov 2023 00:00:00 GMT", "%a, %d %b %Y %H:%M:%S %Z")
+METADATA = {
+    "process": "test_process",
+    "product": "konstellation",
+    "version": "v0.0.1",
+    "workflow": "test_workflow",
+}
+EXPECTED_OBJECT_INFO = ObjectInfo(
+    key="test-key",
+    version="test-version",
+    metadata=METADATA,
+    expires=EXPIRE_DATETIME,
+)
+
+EXPECTED_OBJECT = Object(
+    key="test-key",
+    version="test-version",
+    metadata=METADATA,
+    expires=EXPIRE_DATETIME,
+    data=b"test-payload",
+)
 
 
 @pytest.fixture(scope="function")
@@ -32,15 +55,31 @@ def m_object() -> urllib3.BaseHTTPResponse:
     object_ = Mock(spec=urllib3.BaseHTTPResponse)
     object_.close.return_value = None
     object_.read.return_value = b"test-payload"
+    object_.headers = {  # for object response from get method
+        "content-length": 12,
+        "content-type": "application/octet-stream",
+        "x-amz-expiration": 'expiry-date="Sat, 18 Nov 2023 00:00:00 GMT", rule-id="ttl-test.txt"',
+        "x-amz-meta-process": METADATA["process"],
+        "x-amz-meta-product": METADATA["product"],
+        "x-amz-meta-version": METADATA["version"],
+        "x-amz-meta-workflow": METADATA["workflow"],
+        "x-amz-version-id": "test-version",
+    }
+    object_.is_dir = False
+    object_.metadata = object_.headers  # for stat_object in list methods
+    object_.http_headers = object_.headers  # for object response from save method
+    object_.object_name = "test-key"
+    object_.version_id = "test-version"
 
     return object_
 
 
+@patch.object(Authentication, "__new__", return_value=Mock(spec=Authentication))
 @patch.object(Minio, "__new__", return_value=Mock(spec=Minio))
-def test_ok(_):
+def test_ok(_, __):
     v.set("minio.endpoint", "test-endpoint")
-    v.set("minio.access_key_id", "test-access-key")
-    v.set("minio.access_key_secret", "test-secret-key")
+    v.set("minio.client_user", "test-access-key")
+    v.set("minio.client_password", "test-secret-key")
     v.set("minio.ssl", False)
     v.set("minio.bucket", "test-minio-bucket")
 
@@ -57,27 +96,43 @@ def test_ko(m_persistent_storage):
         assert m_persistent_storage.minio_client is None
 
 
-def test_save_ok(m_persistent_storage):
-    m_persistent_storage.minio_client.put_object.return_value = None
+def test_save_ok(m_persistent_storage, m_object):
+    v.set("metadata.product_id", METADATA["product"])
+    v.set("metadata.workflow_name", METADATA["workflow"])
+    v.set("metadata.process_name", METADATA["process"])
+    v.set("metadata.version_tag", METADATA["version"])
+    m_persistent_storage.minio_client.put_object.return_value = m_object
     payload = io.BytesIO(b"test-payload")
 
-    m_persistent_storage.save("test-key", payload, TTL_DAYS)
+    response = m_persistent_storage.save("test-key", payload, TTL_DAYS)
 
+    m_persistent_storage.minio_client.get_bucket_lifecycle.assert_called_once()
+    m_persistent_storage.minio_client.set_bucket_lifecycle.assert_called_once()
     m_persistent_storage.minio_client.put_object.assert_called_once()
+    assert response == EXPECTED_OBJECT_INFO
 
 
-def test_save_no_ttl_ok(m_persistent_storage):
-    m_persistent_storage.minio_client.put_object.return_value = None
+def test_save_no_ttl_ok(m_persistent_storage, m_object):
+    v.set("metadata.product_id", METADATA["product"])
+    v.set("metadata.workflow_name", METADATA["workflow"])
+    v.set("metadata.process_name", METADATA["process"])
+    v.set("metadata.version_tag", METADATA["version"])
+    m_persistent_storage.minio_client.put_object.return_value = m_object
     payload = io.BytesIO(b"test-payload")
 
-    m_persistent_storage.save("test-key", payload)
+    response = m_persistent_storage.save("test-key", payload)
 
+    m_persistent_storage.minio_client.get_bucket_lifecycle.assert_not_called()
+    m_persistent_storage.minio_client.set_bucket_lifecycle.assert_not_called()
     m_persistent_storage.minio_client.put_object.assert_called_once_with(
         "test-minio-bucket",
         "test-key",
         payload,
         payload.getbuffer().nbytes,
+        metadata=METADATA,
     )
+
+    assert response == EXPECTED_OBJECT_INFO
 
 
 def test_save_ko(m_persistent_storage):
@@ -87,6 +142,8 @@ def test_save_ko(m_persistent_storage):
     with pytest.raises(FailedToSaveFileError):
         m_persistent_storage.save("test-key", payload, TTL_DAYS)
 
+    m_persistent_storage.minio_client.get_bucket_lifecycle.assert_called_once()
+    m_persistent_storage.minio_client.set_bucket_lifecycle.assert_called_once()
     m_persistent_storage.minio_client.put_object.assert_called_once()
 
 
@@ -94,20 +151,18 @@ def test_save_ko(m_persistent_storage):
 def test_get_ok(_, m_persistent_storage, m_object):
     m_persistent_storage.minio_client.get_object.return_value = m_object
 
-    payload, ok = m_persistent_storage.get("test-key", "test-version")
+    payload = m_persistent_storage.get("test-key", "test-version")
 
     m_persistent_storage.minio_client.get_object.assert_called_once()
-    assert payload == m_object.read.return_value
-    assert ok
+    assert payload == EXPECTED_OBJECT
 
 
 @patch("sdk.persistent_storage.persistent_storage.PersistentStorage._object_exist", return_value=False)
 def test_get_not_found_ok(_, m_persistent_storage):
-    payload, ok = m_persistent_storage.get("test-key", "test-version")
+    payload = m_persistent_storage.get("test-key", "test-version")
 
     m_persistent_storage.minio_client.get_object.assert_not_called()
     assert payload is None
-    assert not ok
 
 
 @patch("sdk.persistent_storage.persistent_storage.PersistentStorage._object_exist", return_value=True)
@@ -148,40 +203,42 @@ def test_delete_ko(_, m_persistent_storage):
     m_persistent_storage.minio_client.remove_object.assert_called_once()
 
 
-def test_list_ok(m_persistent_storage):
-    m_persistent_storage.minio_client.list_objects.return_value = [Mock(object_name="test-key")]
+def test_list_ok(m_persistent_storage, m_object):
+    m_persistent_storage.minio_client.list_objects.return_value = [m_object]
+    m_persistent_storage.minio_client.stat_object.side_effect = [m_object]
 
-    keys = m_persistent_storage.list()
+    objects = m_persistent_storage.list()
 
     m_persistent_storage.minio_client.list_objects.assert_called_once()
-    assert keys == ["test-key"]
+    assert objects == [EXPECTED_OBJECT_INFO]
 
 
 def test_list_ko(m_persistent_storage):
     m_persistent_storage.minio_client.list_objects.side_effect = Exception
 
-    keys = m_persistent_storage.list()
+    objects = m_persistent_storage.list()
 
     m_persistent_storage.minio_client.list_objects.assert_called_once()
-    assert keys == []
+    assert objects == []
 
 
-def test_list_versions_ok(m_persistent_storage):
-    m_persistent_storage.minio_client.list_objects.return_value = [Mock(object_name="test-key")]
+def test_list_versions_ok(m_persistent_storage, m_object):
+    m_persistent_storage.minio_client.list_objects.return_value = [m_object]
+    m_persistent_storage.minio_client.stat_object.side_effect = [m_object]
 
-    keys = m_persistent_storage.list_versions("test-key")
+    objects = m_persistent_storage.list_versions("test-key")
 
     m_persistent_storage.minio_client.list_objects.assert_called_once()
-    assert keys == ["test-key"]
+    assert objects == [EXPECTED_OBJECT_INFO]
 
 
 def test_list_versions_ko(m_persistent_storage):
     m_persistent_storage.minio_client.list_objects.side_effect = Exception
 
-    keys = m_persistent_storage.list_versions("test-key")
+    objects = m_persistent_storage.list_versions("test-key")
 
     m_persistent_storage.minio_client.list_objects.assert_called_once()
-    assert keys == []
+    assert objects == []
 
 
 def test__object_exist_ok(m_persistent_storage):
@@ -200,3 +257,29 @@ def test__object_exist_ko(m_persistent_storage):
         m_persistent_storage._object_exist("test-key", "test-version")
 
         m_persistent_storage.minio_client.stat_object.assert_called_once()
+
+
+def test__get_expiry_date_ok(m_persistent_storage, m_object):
+    expiry_date = m_persistent_storage._get_expiry_date(m_object.metadata.get("x-amz-expiration"))
+
+    assert expiry_date == EXPIRE_DATETIME
+
+
+def test__get_expiry_date_empty_ok(m_persistent_storage, m_object):
+    m_object.metadata = {}
+    expiry_date = m_persistent_storage._get_expiry_date(m_object.metadata.get("x-amz-expiration"))
+
+    assert expiry_date is None
+
+
+def test__get_expiry_date_no_match_ok(m_persistent_storage, m_object):
+    m_object.metadata = {"x-amz-expiration": 'expiry="Sat, 18 Nov 2023 00:00:00 GMT"'}
+    expiry_date = m_persistent_storage._get_expiry_date(m_object.metadata.get("x-amz-expiration"))
+
+    assert expiry_date is None
+
+
+def test__process_raw_metadata_ok(m_persistent_storage, m_object):
+    metadata = m_persistent_storage._process_raw_metadata(m_object.metadata)
+
+    assert metadata == METADATA
