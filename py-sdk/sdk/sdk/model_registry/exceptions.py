4c923b95a77a20c0447b7ee3d6329c7d819f34d0
kafkaphoenix <javier.aguilera@intelygenz.com>
2023-12-15 09:01:21 +0100

    Fix/get version registry missing logic (#159)
    Co-authored-by: David Fuentes <51330112+dfinteligenz@users.noreply.github.com>

--- py-sdk/sdk/sdk/model_registry/exceptions.py
+++ py-sdk/sdk/sdk/model_registry/exceptions.py
@@ -54,25 +54,7 @@ class MissingBucketError(Exception):
         super().__init__(f"bucket: {bucket} not found in the persistent storage")
 
 
-class FailedToListFilesError(Exception):
-    def __init__(self, bucket: str, error: Optional[Exception] = None):
-        message = f"failed to list files from the persistent storage bucket {bucket}"
-        super().__init__(f"{message}: {error}" if error else message)
-
-
-class FailedToGetFileError(Exception):
-    def __init__(self, key: str, version: str, bucket: str, error: Optional[Exception] = None):
-        message = f"failed to get file {key} with version {version} from the persistent storage bucket {bucket}"
-        super().__init__(f"{message}: {error}" if error else message)
-
-
-class FailedToSaveFileError(Exception):
-    def __init__(self, key: str, bucket: str, error: Optional[Exception] = None):
-        message = f"failed to save file {key} to the persistent storage bucket {bucket}"
-        super().__init__(f"{message}: {error}" if error else message)
-
-
-class FailedToDeleteFileError(Exception):
-    def __init__(self, key: str, version: str, bucket: str, error: Optional[Exception] = None):
-        message = f"failed to delete file {key} with version {version} from the persistent storage bucket {bucket}"
+class ModelNotFoundError(Exception):
+    def __init__(self, name: str, version: str, error: Optional[Exception] = None):
+        message = f"model {name} with version {version} not found"
         super().__init__(f"{message}: {error}" if error else message)
