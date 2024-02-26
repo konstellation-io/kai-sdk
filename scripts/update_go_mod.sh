3210d31a6cd23ba3d4c1fb6a97607972edb98d9b
Javier Aguilera <jaguilerapuerta@gmail.com>
2024-02-26 11:43:52 +0100

    fix: outdates scripts after deleting demo folder, bump opentelemetry dependency

--- scripts/update_go_mod.sh
+++ scripts/update_go_mod.sh
@@ -4,35 +4,3 @@ echo "Updating go-sdk"
 cd go-sdk
 rm go.sum
 go mod tidy
-
-echo "Updating demo"
-
-echo "Updating cronjob-trigger"
-cd ../demo/cronjob-trigger
-rm go.sum
-go mod tidy
-
-echo "Updating exit"
-cd ../exit
-rm go.sum
-go mod tidy
-
-echo "Updating nats-trigger"
-cd ../nats-trigger
-rm go.sum
-go mod tidy
-
-echo "Updating rest-trigger"
-cd ../rest-trigger
-rm go.sum
-go mod tidy
-
-echo "Updating metrics"
-cd ../metrics
-rm go.sum
-go mod tidy
-
-echo "Updating task"
-cd ../task
-rm go.sum
-go mod tidy
--- scripts/update_poetries.sh
+++ scripts/update_poetries.sh
@@ -5,29 +5,3 @@ cd py-sdk/sdk
 poetry update
 cd ..
 poetry update
-
-echo "Updating demo"
-
-echo "Updating cronjob-trigger-py"
-cd ../demo/cronjob-trigger-py
-poetry update
-
-echo "Updating exit-py"
-cd ../exit-py
-poetry update
-
-echo "Updating nats-trigger-py"
-cd ../nats-trigger-py
-poetry update
-
-echo "Updating metrics-py"
-cd ../metrics-py
-poetry update
-
-echo "Updating task-py"
-cd ../task-py
-poetry update
-
-echo "Updating redis-py"
-cd ../redis-py
-poetry update
